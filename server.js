f2aa338f8f71885b5de45c49d268f070f9b43ef4
Dikewonsi <91637528+Dikewonsi@users.noreply.github.com>
2026-03-07 00:16:50 +0100

    server.js routing to files
    index.html, about.html all open when visited on the browser through the server.

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
+
+//Get current path and file name
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 const server = http.createServer(async (req, res) => {
     try {
         // Check if GET req
         if (req.method == 'GET'){
+            let filePath;
             if (req.url === '/') {
-                res.writeHead(200, { 'Content-Type': 'text/html'});
-                res.end('<h>Homepage</h>');
+                filePath = path.join(__dirname, 'public', 'index.html');
             } else if (req.url === '/about') {
-                res.writeHead(200, { 'Content-Type': 'text/html'});
-                res.end('<h>About Us</h>');
+                filePath = path.join(__dirname, 'public', 'about.html');
             } else {
-                res.writeHead(404, { 'Content-Type': 'text/html'});
-                res.end('<h>Not Found</h>');
+                throw new Error('Not Found');
             }
+
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+
+            
         } else {
             throw new Error('Method not allowed');
         }
